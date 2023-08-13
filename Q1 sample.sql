858034689e0bdd981547fcde82d606571c62bd4a
Jayarajurevu <142162110+Jayarajurevu@users.noreply.github.com>
2023-08-13 20:06:16 +0530

    Add files via upload

---
+++ Q1 sample.sql
@@ -0,0 +1,8 @@
+USE AdventureWorks2022;
+SELECT
+CustomerID,
+StoreID,
+AccountNumber
+FROM Sales.Customer;
+/*Write a SELECT statement that lists the customers. Include the CustomerID, StoreID, and 
+AccountNumber columns from the Sales.Customer table*/
